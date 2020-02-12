284bba709ca04076f550c8fcb45ac09e83146ce7
Gabby Jones <jones.gm90@gmail.com>
2020-02-12 10:39:27 -0500

    functioning add and view

---
+++ employeeTracker.sql
@@ -0,0 +1,57 @@
+DROP database IF exists employeeTracker;
+CREATE DATABASE employeeTracker; 
+USE employeeTracker; 
+
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+/*CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL 
+  
+   ); 
+
+CREATE TABLE role (
+  INDEX dep_ind (department_id),
+  title VARCHAR(30) NOT NULL, 
+  salary DECIMAL(10,2) NOT NULL, 
+  department_id  INT NOT NULL, 
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id)
+  ON DELETE CASCADE 
+  );
+
+CREATE TABLE employee(
+  first_name VARCHAR(30) NOT NULL, 
+  last_name VARCHAR(30) NOT NULL, 
+  role_id INT NOT NULL, 
+  INDEX role_ind (role_id),
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES employee(id)
+  ON DELETE SET NULL,
+  CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id)
+  ON DELETE CASCADE   
+  )*/
+  
--- index.js
+++ index.js
@@ -21,13 +21,41 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  choseView(); 
+  choseAdd(); 
 });
 
 //sets up express
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//funtion to take users back to "main menu" at the end of view, add, or update
+
+function mainMenu(){
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: ["Add", "View", "or update employees"]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+          case "Add":
+            choseAdd();
+            break;
+  
+          case "View":
+            choseView();
+            break;
+  
+          case "or update employees":
+            updateEmployee();
+            break;
+        }
+      });
+  }
+
 
 //handles user choice for what they'd like to view
 
@@ -63,9 +91,10 @@ function viewDepartments(){
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    connection.end();
+    then
+    mainMenu(); 
   });
-  choseAdd()
+  
 };
 
 function viewRoles(){
@@ -74,20 +103,21 @@ function viewRoles(){
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    connection.end();
+    then
+    mainMenu(); 
   });
-  choseAdd()
-};
+}; 
 
-function viewEmployees(){
+
+async function viewEmployees(){
   console.log("Selecting all employees...\n");
   connection.query("SELECT * FROM employee", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    connection.end();
+    then
+    mainMenu(); 
   });
-  choseAdd()
 }
 //handles user choice for what they'd like to add
 
@@ -131,6 +161,7 @@ function addDepartment() {
         }
         console.log("this worked", result);
         console.table(result)
+        mainMenu();
       });
     });
 }
@@ -156,13 +187,20 @@ function addRole() {
       }
     ])
     .then(function(answer) {
-      let query = "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)";
-      connection.query(query, [answer.role, answer.salary, answer.departmentId], function(err, result) {
-        if (err) {
-          throw err;
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          title: answer.role,
+          salary: answer.salary,
+          department_id: answer.departmentId
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Your role was created successfully!");  
+          console.table(answer)
+          mainMenu();
         }
-        console.log("this worked", result);
-      });
+      );
     });
 }
 
@@ -201,136 +239,11 @@ function addEmployee() {
             throw err;
           }
           console.log("this worked", result);
+          console.table(result)
+          mainMenu();
         }
       );
     });
 }
 
-// function multiSearch() {
-//   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-//   connection.query(query, function(err, res) {
-//     for (var i = 0; i < res.length; i++) {
-//       console.log(res[i].artist);
-//     }
-//     runSearch();
-//   });
-// }
 
-// function rangeSearch() {
-//   inquirer
-//     .prompt([
-//       {
-//         name: "start",
-//         type: "input",
-//         message: "Enter starting position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       },
-//       {
-//         name: "end",
-//         type: "input",
-//         message: "Enter ending position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       var query =
-//         "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//       connection.query(query, [answer.start, answer.end], function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             "Position: " +
-//               res[i].position +
-//               " || Song: " +
-//               res[i].song +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Year: " +
-//               res[i].year
-//           );
-//         }
-//         runSearch();
-//       });
-//     });
-// }
-
-// function songSearch() {
-//   inquirer
-//     .prompt({
-//       name: "song",
-//       type: "input",
-//       message: "What song would you like to look for?"
-//     })
-//     .then(function(answer) {
-//       console.log(answer.song);
-//       connection.query(
-//         "SELECT * FROM top5000 WHERE ?",
-//         { song: answer.song },
-//         function(err, res) {
-//           console.log(
-//             "Position: " +
-//               res[0].position +
-//               " || Song: " +
-//               res[0].song +
-//               " || Artist: " +
-//               res[0].artist +
-//               " || Year: " +
-//               res[0].year
-//           );
-//           runSearch();
-//         }
-//       );
-//     });
-// }
-
-// function songAndAlbumSearch() {
-//   inquirer
-//     .prompt({
-//       name: "artist",
-//       type: "input",
-//       message: "What artist would you like to search for?"
-//     })
-//     .then(function(answer) {
-//       var query =
-//         "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-//       query +=
-//         "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-//       query +=
-//         "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-
-//       connection.query(query, [answer.artist, answer.artist], function(
-//         err,
-//         res
-//       ) {
-//         console.log(res.length + " matches found!");
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             i +
-//               1 +
-//               ".) " +
-//               "Year: " +
-//               res[i].year +
-//               " Album Position: " +
-//               res[i].position +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Song: " +
-//               res[i].song +
-//               " || Album: " +
-//               res[i].album
-//           );
-//         }
-
-//         runSearch();
-//       });
-//     });
-// }
